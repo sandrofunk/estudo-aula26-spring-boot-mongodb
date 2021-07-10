7b880f3c843a059125f511d9b31191042654fdb6
sandrofunk <sagfunk@gmail.com>
2021-07-10 17:08:53 -0300

    Projeto Criado

---
+++ src/test/java/com/example/pep/Estudoaula26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Estudoaula26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
